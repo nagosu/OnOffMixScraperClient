254b8431155262ce34870d50129edefc6f7ddc04
nagosu <nozick1021@gmail.com>
2024-10-31 23:39:54 +0900

    이메일 구독 API 연동 완료

---
+++ static/js/index.js
@@ -0,0 +1,31 @@
+const emailForm = document.getElementById('emailForm');
+
+emailForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+
+  try {
+    const response = await fetch(
+      'https://on-off-mix-scraper.vercel.app/api/email',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert('구독 신청이 완료되었습니다.');
+    } else {
+      alert('등록에 실패했습니다. 다시 시도해주세요.');
+    }
+  } catch (e) {
+    console.error(e);
+    alert('오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+  }
+});
